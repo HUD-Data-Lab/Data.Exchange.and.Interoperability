5addf759c31ccc5419c9558821c779d6792010cf
Grant T. <grant.tuioti@icf.com>
2026-07-16 12:55:32 -0700

    Set up for R shiny with ontology

--- Translation Layer/scripts/SPARQL_ontology_translator.R
+++ Translation Layer/scripts/SPARQL_ontology_translator.R
@@ -1,10 +1,9 @@
 #HMIS Enumerated fields
 
-
 library(rdflib)
-
+library(yaml)
 # Load ontology
-g <- rdf_parse("Ontology Generator/output/Output_20260710_165301/hmis_ontology0710T1653_01.ttl", format = "turtle")
+g <- rdf_parse("Artifacts/Ontology/Output_v1.0.0-beta/hmis_ontologyv1.0.0-beta.ttl", format = "turtle")
 
 ### Be able to query and pull all scalar and enumerated fields from the ontology ----
 
@@ -57,16 +56,96 @@ clean_MetaData <- metadata %>%
     field_type = str_replace(field_type, "^.*[#/]", "")
   )
 
+
+
+
+field <- clean_MetaData %>%
+  filter(property == "LivingSituation")
+
+enum_vals <- vocab_values %>%
+  filter(scheme == field$scheme[1]) %>% 
+  pull(concept)
+
+schema <- list(
+  type = "string",
+  enum = enum_vals,
+  `x-hmis-vocabulary` = field$dataElementNumberAndField[1]
+)
+
+field_yaml <- list(
+  CurrentEdStatus = schema
+)
+
+cat(as.yaml(schema))
+
+
+
 HMIS_Dictionary_Options <- clean_MetaData %>% 
   select(dataDictionaryName,dataElementNumberAndField) %>% 
   filter(dataDictionaryName != "NA")
 
 view(get_field_definition(
   "Data Element Name", #Options here are "Data Element Name" OR "Data Element Number"
-  "HouseholdType", #Query removed all spaces when pulling the name.
+  "LivingSituation", #Query removed all spaces when pulling the name.
   clean_MetaData,
   g
 ))
+
+
+jsonld_context <- clean_MetaData %>%
+  transmute(
+    term = property,
+    iri = paste0("hmis:", property)
+  )
+
+
+resources <- clean_MetaData %>%
+  group_by(domain)
+
+## YAML file generation
+query_vocab <- '
+PREFIX hmis: <http://www.semanticweb.org/61084/ontologies/2026/2/hmis#>
+PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+
+SELECT ?scheme ?concept ?label ?notation
+WHERE {
+
+  ?concept skos:inScheme ?scheme .
+
+  OPTIONAL { ?concept skos:prefLabel ?label . }
+  OPTIONAL { ?concept skos:notation ?notation . }
+}
+'
+
+vocab_values <- rdf_query(g, query_vocab) %>%
+  mutate(
+    #scheme = str_replace(scheme, "^.*[#/]", ""),
+    concept = str_replace(concept, "^.*[#/]", "")
+  )
+
+enrollment_schema <- build_resource_schema(
+  fields = c(
+    "HouseholdID",
+    "CurrentEdStatus",
+    "VeteranStatus"
+  ),
+  metadata = clean_MetaData,
+  vocab_values = vocab_values
+)
+
+openapi_object <- list(
+  components = list(
+    schemas = list(
+      Enrollment = enrollment_schema
+    )
+  )
+)
+
+cat(as.yaml(openapi_object))
+
+
+
+
 
 
 
